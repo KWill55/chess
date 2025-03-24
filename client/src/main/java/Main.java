a1b40a41b20e9041de4eb2d533046fecb5d22e72
Kendal Williams <kendalwilliams05@gmail.com>
2025-03-24 16:19:01 -0600

    implement postlogin repl -- still having pom.xml bug

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,13 @@
 import chess.*;
+import client.Repl;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        int port = 8080; // Match this with your server port
+        Repl repl = new Repl(port);
+        repl.run();
     }
 }
--- client/src/main/java/client/PostLoginRepl.java
+++ client/src/main/java/client/PostLoginRepl.java
@@ -1,4 +1,42 @@
 package client;
 
+import exception.ResponseException;
+
+import java.util.Scanner;
+
 public class PostLoginRepl {
+    private final ChessClient client;
+
+    public PostLoginRepl(ChessClient client) {
+        this.client = client;
+    }
+
+    public void run() {
+        System.out.println("\n== Post-login Commands ==");
+        System.out.println(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        String result = "";
+
+        while (!result.equals("logout") && !result.equals("quit")) {
+            System.out.print("\n>>> ");
+            String input = scanner.nextLine();
+
+            try {
+                result = client.eval(input);
+
+                if (result.equals("logout")) {
+                    System.out.println("Logged out.");
+                    break;
+                } else if (result.equals("quit")) {
+                    System.out.println("Goodbye.");
+                    break;
+                }
+
+                System.out.print(result);
+            } catch (Exception e) {
+                System.out.println("Unexpected error: " + e.getMessage());
+            }
+        }
+    }
 }
