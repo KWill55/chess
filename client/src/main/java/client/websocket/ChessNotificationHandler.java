0775a21930cd2756b16a7c3e14e970746229e6a0
Kendal Williams <kendalwilliams05@gmail.com>
2025-03-26 14:20:39 -0600

    updating for code quality

--- client/src/main/java/client/websocket/ChessNotificationHandler.java
+++ client/src/main/java/client/websocket/ChessNotificationHandler.java
@@ -3,10 +3,6 @@ package client.websocket;
 import websocketmessages.Notification;
 
 public class ChessNotificationHandler implements NotificationHandler {
-    public void notify(Notification notification) {
-        // Placeholder for now
-        System.out.println("[NOTIFICATION] " + notification.message());
-    }
 }
 
 
