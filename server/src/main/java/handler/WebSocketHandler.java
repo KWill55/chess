707efc45a5e05c7f160ab83a65e389842e27327e
Kendal Williams <kendalwilliams05@gmail.com>
2025-04-07 22:33:05 -0600

    implement joinGame message websocket using username

--- server/src/main/java/handler/WebSocketHandler.java
+++ server/src/main/java/handler/WebSocketHandler.java
@@ -20,7 +20,7 @@ public class WebSocketHandler {
 
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws IOException {
-        System.out.println("📩 Incoming WebSocket message: " + message);
+//        System.out.println("Incoming WebSocket message: " + message);
 
         UserGameCommand command = gson.fromJson(message, UserGameCommand.class);
         switch (command.getCommandType()) {
@@ -35,31 +35,34 @@ public class WebSocketHandler {
         int gameID = command.getGameID();
         String authToken = command.getAuthToken();
 
-        System.out.println("📥 CONNECT received for gameID: " + gameID + ", authToken: " + authToken);
+//        System.out.println("CONNECT received for gameID: " + gameID + ", authToken: " + authToken);
 
         // Add the session to the game
         gameSessions.putIfAbsent(gameID, new ArrayList<>());
         gameSessions.get(gameID).add(session);
 
-        System.out.println("📌 Sessions in game " + gameID + ": " + gameSessions.get(gameID).size());
+//        System.out.println(" Sessions in game " + gameID + ": " + gameSessions.get(gameID).size());
 
         try {
             GameDAO gameDAO = new SQLGameDAO();
             GameData gameData = gameDAO.getGame(gameID);
 
+            AuthDAO authDAO = new SQLAuthDAO();
+            String username = authDAO.getAuth(authToken).username();
+
             ServerMessage loadGame = new LoadGameMessage(gameData.game());
             session.getRemote().sendString(gson.toJson(loadGame));
 
-            String notificationText = gameData.gameName() + " - " + authToken + " joined the game";
+            String notificationText = gameData.gameName() + " - " + username + " joined the game";
             ServerMessage notify = new NotificationMessage(notificationText);
 
-            System.out.println("📣 Notification to send: " + notificationText);
+//            System.out.println("Notification to send: " + notificationText);
 
             for (Session s : gameSessions.get(gameID)) {
-                System.out.println("➡️ Attempting to send to session: " + s);
+//                System.out.println("Attempting to send to session: " + s);
                 if (s.isOpen() && !s.equals(session)) {
                     s.getRemote().sendString(gson.toJson(notify));
-                    System.out.println("✅ Sent to other client!");
+//                    System.out.println("Sent to other client!");
                 }
             }
 
